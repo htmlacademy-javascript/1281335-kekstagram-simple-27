a3bb997730653943717234814ac44862d551f134
Kseniya Kozlova <kseniya.kozlova.tl@gmail.com>
2022-11-05 22:18:10 +0300

    fixes eslint errors

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function getRandomNumber(min, max) {
   if (
-    typeof min !== "number" ||
-    typeof max !== "number" ||
+    typeof min !== 'number' ||
+    typeof max !== 'number' ||
     min < 0 ||
     max < 0 ||
     min === max ||
@@ -13,12 +13,14 @@ function getRandomNumber(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
+
 console.log(getRandomNumber(5.5, 10.7));
 
 function getStringLength(string, maxLength) {
-  if (typeof maxLength !== "number") {
+  if (typeof maxLength !== 'number') {
     return false;
   }
   return string.length <= maxLength;
 }
-console.log(getStringLength("", ""));
+
+console.log(getStringLength('', ''));
