e96a283b88ff40dba4ab42844a8e49426ad82b5d
Kseniya Kozlova <kseniya.kozlova.tl@gmail.com>
2022-11-05 21:36:13 +0300

    adds getRandomNumber and getStringLength functions

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function getRandomNumber(min, max) {
+  if (
+    typeof min !== "number" ||
+    typeof max !== "number" ||
+    min < 0 ||
+    max < 0 ||
+    min === max ||
+    min > max
+  ) {
+    return NaN;
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+console.log(getRandomNumber(5.5, 10.7));
+
+function getStringLength(string, maxLength) {
+  if (typeof maxLength !== "number") {
+    return false;
+  }
+  return string.length <= maxLength;
+}
+console.log(getStringLength("", ""));
