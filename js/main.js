a3b1fbff5d3bd451e3bbd43e2bc931cf50c6bc49
Kseniya Kozlova <kseniya.kozlova.tl@gmail.com>
2022-11-07 00:59:54 +0300

    fixes getRandomNumber function

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
-function getRandomNumber(min, max) {
-  if (
-    typeof min !== 'number' ||
-    typeof max !== 'number' ||
-    min < 0 ||
-    max < 0 ||
-    min === max ||
-    min > max
-  ) {
+const getRandomNumber = (min, max) => {
+  if (min >= 0  && max >= 0) {
+    if (min > max) {
+      [min, max] = [max, min];
+    }
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  else {
     return NaN;
   }
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
-console.log(getRandomNumber(5.5, 10.7));
+console.log(getRandomNumber(-5.5, 10.7));
 
-function getStringLength(string, maxLength) {
+const getStringLength = (string, maxLength) => {
   if (typeof maxLength !== 'number') {
     return false;
   }
